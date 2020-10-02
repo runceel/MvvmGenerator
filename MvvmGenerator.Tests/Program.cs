8a1f4742ce7f70e104d46309ffc7d2cd1706a569
Kazuki Ota <kaota@microsoft.com>
2020-10-02 11:01:38 +0900

    add hello world generator

--- MvvmGenerator.Tests/Program.cs
+++ MvvmGenerator.Tests/Program.cs
@@ -6,7 +6,7 @@ namespace MvvmGenerator.Tests
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MvvmGenerator.Greeter.Greet();
         }
     }
 }
---
+++ MvvmGenerator/AutoImplementNotifyPropertyChangedGenerator.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvvmGenerator
+{
+    [Generator]
+    public class AutoImplementNotifyPropertyChangedGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            context.AddSource("hello", SourceText.From($@"
+using System;
+namespace MvvmGenerator
+{{
+    public static class Greeter
+    {{
+        public static void Greet() => Console.WriteLine(""Hello world"");
+    }}
+}}", Encoding.UTF8));
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+        }
+    }
+}
