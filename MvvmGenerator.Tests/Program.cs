3cf3ed6ee18ee6993af3a68eb3162b15535a5f25
Kazuki Ota <kaota@microsoft.com>
2020-10-02 10:51:01 +0900

    add projects

---
+++ MvvmGenerator.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MvvmGenerator.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
